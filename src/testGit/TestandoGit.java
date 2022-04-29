0c1d92597d6ed53f6be7236433a9e5777b58e5d4
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2022-04-29 10:35:30 -0300

    First commit

---
+++ src/testGit/TestandoGit.java
@@ -0,0 +1,16 @@
+package testGit;
+
+
+import testGit.model.Gato;
+
+public class TestandoGit {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        int a = 5;
+        int b = 6;
+        System.out.println("Hello World!" + (a + b));
+    }
+}
